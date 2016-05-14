38a217056bd8acfb3da87eb04d8278a38513beae
JordPWN <jordan.ratanadawong@gmail.com>
2016-05-14 16:40:46 +0000

    Step 5

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,12 @@
-# Homepage (Root path)
 get '/' do
   erb :index
+end
+
+get '/messages' do
+  @messages = Message.all
+  erb :'messages/index'
+end
+
+get '/messages/new' do
+  erb :'messages/new'
 end
